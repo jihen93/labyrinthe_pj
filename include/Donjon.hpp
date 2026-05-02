20771de3d06fe63012038bb933f4f86b00479ef0
Cyril856 <cyrildemontlaur@gmail.com>
2026-05-02 18:21:29 +0200

    Debug main.cpp

--- include/Donjon.hpp
+++ include/Donjon.hpp
@@ -1,14 +1,17 @@
 #pragma once
 #include <iostream>
+#include "Case.hpp"
+#include <vector>  
+using namespace std;
 
 class Donjon {
-    vector < vector < Case * > > grille ;
+    vector<vector<Case*>>grille ;
     int largeur;
     int hauteur;
 public :
     void generer ( int largeur , int hauteur ) ;
     void afficher () ;
-    vector < pair < int , int > > trouverChemin ( /* ...  */) ;
+    //vector < pair < int , int > > trouverChemin ( /* ...  */) ;
 };  
 
 /* Affichage ASCII du donjon :
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include "Donjon.hpp"
-#include "Mur.hpp"
-#include "Tresor.hpp"
-#include "Monstre.hpp"
+#include "include/Donjon.hpp"
+#include "include/Mur.hpp"
+#include "include/Tresor.hpp"
+#include "include/Monstre.hpp"
 #include <iostream>
+#include <utility> // Pas oublier, sinon le main ne renvoie rien !
+
+using namespace std;
     
 int main() {
 
@@ -13,10 +16,10 @@ int main() {
     //donjon.generer(20,20);
     //donjon.afficher();
 
-    //cout << "Affichage des cases test :" << endl;
-    //cout << "Mur : " << m.afficher() << endl;
-    //cout << "Tresor : " << t.afficher() << " (Valeur: " << t.getValeur() << ")" << endl;
-    //cout << "Monstre : " << mon.afficher() << " (PV: " << mon.getPv() << ")" << endl;
+    cout << "Affichage des cases test :" << endl;
+    cout << "Mur : " << m.afficher() << endl;
+    cout << "Tresor : " << t.afficher() << " (Valeur: " << t.getValeur() << ")" << endl;
+    cout << "Monstre : " << mon.afficher() << " (PV: " << mon.getPv() << ")" << endl;
     
     return 0;
 }
